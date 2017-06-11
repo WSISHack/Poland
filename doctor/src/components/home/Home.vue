57113985816681e694d731d9b51d6c84fb252159
JKrol <github@kroljakub.pl>
2017-06-11 22:44:55 +0200

    Split for two projects

---
+++ doctor/src/components/home/Home.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    <air-info></air-info>
+    <charts></charts>
+  </div>
+</template>
+
+<script>
+import Charts from './Charts';
+import AirInfo from './AirInfo';
+
+export default {
+  name: 'home',
+  created () {
+    this.$store.commit('setTitle', 'Dashboard')
+  },
+  data () {
+    return {
+    }
+  },
+  components: { Charts, AirInfo }
+}
+</script>
+
+<style scoped>
+</style>
