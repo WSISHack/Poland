e70715f1409231eff6dc28956f49942800a81b24
robi24 <robi240@gmail.com>
2017-06-11 11:27:01 +0200

    Added jquery and OpenFoodApi template

---
+++ src/components/OpenFoodApi/OpenFoodApi.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    	test
+  </div>
+</template>
+
+<script>
+import jqery from 'jquery'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  actions: {
+    getProduct(barcode) {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
