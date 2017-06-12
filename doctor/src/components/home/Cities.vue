189496dc3da92349cd8a7f45dd02f2c8224ef9e3
robi24 <robi240@gmail.com>
2017-06-12 08:55:26 +0200

    Fixed warnings in doctor app

--- doctor/src/components/home/Cities.vue
+++ doctor/src/components/home/Cities.vue
@@ -3,7 +3,7 @@
     <search></search>
     <div class="phone-viewport">
       <md-list class="md-double-line">
-        <md-list-item v-for="city in cities" @click.native="navigate()" class="md-inset">
+        <md-list-item v-for="(city, id) in cities" :key="id" @click.native="navigate()" class="md-inset">
           <div class="md-list-text-container">
             <span>{{city.name}}</span>
             <span>Patients: {{city.patients}}</span>
