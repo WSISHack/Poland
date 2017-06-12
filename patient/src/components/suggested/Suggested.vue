43dda8b1973ac51acb63a49c88722d20b394c1c4
olzone <osurman@gmail.com>
2017-06-12 09:59:12 +0200

    machine learning mockup

--- patient/src/components/suggested/Suggested.vue
+++ patient/src/components/suggested/Suggested.vue
@@ -21,16 +21,98 @@
       </md-table-header>
 
       <md-table-body>
-        <md-table-row v-for="(row, index) in 15" :key="index">
+        <md-table-row>
           <md-table-cell>
-            <p>Mleko słonia</p>
-            <p><span class="tab-prop-1">66g</span> <span class="tab-prop-2 prop-right">17g</span></p>
+            <p>Cauliflower</p>
+            <p><span class="tab-prop-1">2.0g</span> <span class="tab-prop-2 prop-right">2.5g</span></p>
           </md-table-cell>
           <md-table-cell md-numeric>
-            <p>10</p>
-            <p class="tab-prop-3">12g</p>
+            <p>20</p>
+            <p class="tab-prop-3">0.0g</p>
           </md-table-cell>
         </md-table-row>
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Spinach</p>
+            <p><span class="tab-prop-1">2.8g</span> <span class="tab-prop-2 prop-right">1.5g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>30</p>
+            <p class="tab-prop-3">0.8g</p>
+          </md-table-cell>
+        </md-table-row>
+
+
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Broccoli</p>
+            <p><span class="tab-prop-1">2.82g</span> <span class="tab-prop-2 prop-right">4.04g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>30</p>
+            <p class="tab-prop-3">0.37g</p>
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Blueberries</p>
+            <p><span class="tab-prop-1">0.6g</span> <span class="tab-prop-2 prop-right">12.2g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>45</p>
+            <p class="tab-prop-3">0.6g</p>
+          </md-table-cell>
+        </md-table-row>
+
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Fish (Atlantic cod)</p>
+            <p><span class="tab-prop-1">17.8g</span> <span class="tab-prop-2 prop-right">0.0g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>82</p>
+            <p class="tab-prop-3">0.0g</p>
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Sweet potatoes</p>
+            <p><span class="tab-prop-1">1.0g</span> <span class="tab-prop-2 prop-right">18.0g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>76</p>
+            <p class="tab-prop-3">0.0g</p>
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Walnuts</p>
+            <p><span class="tab-prop-1">16.0g</span> <span class="tab-prop-2 prop-right">18.0g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>645</p>
+            <p class="tab-prop-3">60.3g</p>
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-cell>
+            <p>Quinoa</p>
+            <p><span class="tab-prop-1">4.0g</span> <span class="tab-prop-2 prop-right">21.0g</span></p>
+          </md-table-cell>
+          <md-table-cell md-numeric>
+            <p>120</p>
+            <p class="tab-prop-3">2.0g</p>
+          </md-table-cell>
+        </md-table-row>
+
+
       </md-table-body>
     </md-table>
   </div>
